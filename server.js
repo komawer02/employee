0d98a1815bb7aa4b66fa306e7ec4ceb87be18ea8
komawer02 <komawer02@naver.com>
2022-07-01 13:06:08 +0900

    firstpush

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.listen(8080, function(req, res){    
+    console.log('listening on 8080');
+
+});
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + "/index.html");
+})

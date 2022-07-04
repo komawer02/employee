ba3c6ee938bea315ad0edb42c3b52f7689f1a646
komawer02 <komawer02@naver.com>
2022-07-04 12:56:21 +0900

    secondcommit

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const MongoClient = require('mongodb').MongoClient
 
-app.listen(8080, function(req, res){    
-    console.log('listening on 8080');
+MongoClient.connect('mongodb+srv://root:042602@cluster0.eatnaco.mongodb.net/?retryWrites=true&w=majority', function(err, client){
+    
+    if(err) return console.log(err);
+    db = client.db('todoapp');
+    
+    app.listen(8080, function(req, res){    
+        console.log('listening on 8080');
+    
+    });
+})
 
-});
+
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + "/index.html");

45c9eeff22179a7e95e32c440273c30d403badc8
komawer02 <komawer02@naver.com>
2022-07-08 14:46:08 +0900

    location mark

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const MongoClient = require('mongodb').MongoClient
 
+const { Navigator } = require("node-navigator");
+const navigator = new Navigator();
+
+
+
 MongoClient.connect('mongodb+srv://root:042602@cluster0.eatnaco.mongodb.net/?retryWrites=true&w=majority', function(err, client){
     
     if(err) return console.log(err);
@@ -21,5 +26,31 @@ app.get('/manage', function(req, res){
     res.sendFile(__dirname + "/join.html");
 })
 app.get('/', function(req, res){
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/test.html");
+    navigator.geolocation.getCurrentPosition((success, error) => {
+        if (error) console.error(error);
+        else console.log(success.latitude, success.longitude);
+        if(calcDistance(success.latitude, success.longitude) < 1000){
+            console.log('안에있음');
+        }
+        else    console.log(calcDistance(success.latitude, success.longitude));
+    });
 })
+
+function calcDistance(lat1, lon1, lat2 = 37.03449586752981, lon2 =  127.05197172911399){
+    var theta = lon1 - lon2;
+    dist = Math.sin(deg2rad(lat1))*Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1))*Math.cos(deg2rad(lat2))*Math.cos(deg2rad(theta));
+    dist = Math.acos(dist);
+    dist = rad2deg(dist);
+    dist = dist * 60 * 1.1515;
+    dist = dist * 1.609344;
+    return (Number(dist*1000).toFixed(2))/4;
+
+}
+function deg2rad(deg){
+    return (deg*Math.PI/180);
+
+}
+function rad2deg(rad){
+    return (rad*180/Math.PI);
+}

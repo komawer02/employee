70309204b2d5410a45726fff382ab164fb847fa9
hongmonmon <komawer11@gmail.com>
2022-08-05 13:58:23 +0900

    sc

--- server.js
+++ server.js
@@ -37,20 +37,23 @@ app.get('/', function(req, res){
     });
 })
 
-function calcDistance(lat1, lon1, lat2 = 37.03449586752981, lon2 =  127.05197172911399){
+function getDistance(lat1, lon1, lat2, lon2) {
+    if ((lat1 == lat2) && (lon1 == lon2))
+        return 0;
+
+    var radLat1 = Math.PI * lat1 / 180;
+    var radLat2 = Math.PI * lat2 / 180;
     var theta = lon1 - lon2;
-    dist = Math.sin(deg2rad(lat1))*Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1))*Math.cos(deg2rad(lat2))*Math.cos(deg2rad(theta));
+    var radTheta = Math.PI * theta / 180;
+    var dist = Math.sin(radLat1) * Math.sin(radLat2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.cos(radTheta);
+    if (dist > 1)
+        dist = 1;
+
     dist = Math.acos(dist);
-    dist = rad2deg(dist);
-    dist = dist * 60 * 1.1515;
-    dist = dist * 1.609344;
-    return (Number(dist*1000).toFixed(2))/4;
+    dist = dist * 180 / Math.PI;
+    dist = dist * 60 * 1.1515 * 1.609344 * 1000;
+    if (dist < 100) dist = Math.round(dist / 10) * 10;
+    else dist = Math.round(dist / 100) * 100;
 
-}
-function deg2rad(deg){
-    return (deg*Math.PI/180);
-
-}
-function rad2deg(rad){
-    return (rad*180/Math.PI);
+    return dist;
 }
